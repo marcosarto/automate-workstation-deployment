021cc7aee5465732030eea2d7a2061873e088dd7
marcosarto <marcosartorelli24@gmail.com>
2022-04-17 17:20:47 +0200

    up

--- packages.ps1
+++ packages.ps1
@@ -1,4 +1,12 @@
 winget install Microsoft.WindowsTerminal
+winget install Git.Git
+winget install Spotify.Spotify
+winget install WhatsApp.WhatsApp
+winget install Microsoft.PowerToys
+winget install calibre.calibre
+winget install Telegram.TelegramDesktop
+winget install Microsoft.VisualStudioCode
+ 
 
 #Windows Terminal
 $terminal=Get-ChildItem -Path ~/AppData/Local/Packages/Microsoft.WindowsTerminal* -Name
