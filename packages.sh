cdbfec8416395f1ace7a0b9ff6d34081a1e089a4
Marco Sartorelli <marco.sartorelli@eng.it>
2022-04-19 14:43:43 +0200

    Added starship

--- packages.sh
+++ packages.sh
@@ -1,3 +1,13 @@
+#Installations
+
+#Fish
+sudo add-apt-repository ppa:fish-shell/release-3
+sudo apt-get update
+sudo apt-get install fish
+
 #Starship installation + config
 curl -sS https://starship.rs/install.sh | sh 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+mkdir -p ~/.config #Create config folder if it doesn't exist for starship
+
